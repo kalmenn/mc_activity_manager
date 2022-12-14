eaf465e431f127c2dfd2d81d9ababfb96eec9cc9
kalmenn <kalmenn@proton.me>
2022-12-14 20:34:24 +0100

    Now capable of following state changes
    - Reading the packet length
    - Pushing the bytes into a buffer
    - Acting on the packet and flushing the buffer when the full packet has been recieved
    
    Signed-off-by: kalmenn <kalmenn@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -21,44 +21,89 @@ enum PacketLength {
     Reading(Varint)
 }
 
-// ça manque de bits
+impl PacketLength {
+    fn to_known(&mut self) {
+        if let Self::Reading(varint) = self {
+            *self = Self::Known(Varint{length: varint.length, data: varint.data});
+        }
+    }
+}
+
+struct Packet {
+    length: PacketLength,
+    body: Vec<u8>
+}
+
+impl Packet {
+    /// Returns an empty packet
+    fn empty() -> Packet {
+        return Packet{
+            length: PacketLength::Reading(Varint{length: 0, data: 0}),
+            body: Vec::<u8>::new()
+        };
+    }
+}
+
+#[derive(Debug)]
+enum RequestState {
+    Status,
+    Login,
+    Handshake
+}
 
 fn handle_connection(mut stream: TcpStream) {
     println!("request from {}\n", &stream.peer_addr().unwrap());
 
-    let mut packet_length = PacketLength::Reading(Varint{length: 0, data: 0 });
+    let mut packet = Packet::empty();
+    
+    let mut request_state = RequestState::Handshake;
 
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    for byte in BufReader::new(&mut stream)
         .bytes()
         .map(|byte| byte.unwrap())
-        .enumerate()
-        .take_while(|byte| {
-            match &mut packet_length {
-                PacketLength::Known(varint) => (byte.0 - varint.length) <= varint.data as usize,
-                PacketLength::Reading(varint) => {
-                    let data = byte.1;
-                    varint.data += ((data & 0b01111111) << (7 * varint.length) ) as u64;
-                    if data >= 128 {
-                        varint.length += 1;
-                    } else {
-                        // println!("varint.length = {} , varint.data = {}", varint.length+1, varint.data);
-                        packet_length = PacketLength::Known(Varint { length: varint.length, data: varint.data });
-                    }
-                    true
+        .enumerate() 
+    {
+        match &mut packet.length {
+            PacketLength::Reading(varint) => {
+                varint.data += ((byte.1 & 0b01111111) << (7 * varint.length) ) as u64;
+                varint.length += 1;
+                if byte.1 < 128 {
+                    packet.length = PacketLength::Known(Varint{length: varint.length, data: varint.data});
                 }
+            },
+            PacketLength::Known(varint) => {
+                packet.body.push(byte.1);
+                if packet.body.len() >= varint.data as usize {
+                    // println!("packet body:{:?}", packet.body);
+                    println!("recieved packet of length: {}", &varint.data);
+                    match request_state {
+                        RequestState::Handshake => {
+                            println!("reached state: Handshake");
+                            match packet.body.iter().last().unwrap_or(&0) {
+                                1 => {
+                                    request_state = RequestState::Status;
+                                    // TODO: set MODT in response
+                                },
+                                2 => {
+                                    request_state = RequestState::Login;
+                                    // TODO: disconnect player and display message
+                                }
+                                _ => return
+                            }
+                        },
+                        RequestState::Login => {
+                            println!("reached state: Login");
+                            return
+                        },
+                        RequestState::Status => {
+                            println!("reached state: Status");
+                            return
+                            // TODO: deal with ping requests
+                        }
+                    }
+                    packet = Packet::empty();
+                };
             }
-        })
-        .map(|byte| byte.1)
-        .collect();
-
-        match http_request.iter().last().unwrap() {
-            0b00000001 => println!("status request"),
-            0b00000010 => println!("login request"),
-            _ => println!("unknown request")
-        }
-
-        // for byte in http_request.into_iter() {
-        //     println!("{:#10b}", byte);
-        // }
+        };
+    }
 }
