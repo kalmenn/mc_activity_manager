706bcc624407a09c79834b15c658512b64c807d9
kalmenn <kalmenn@proton.me>
2022-12-16 00:31:18 +0100

    wrapped calls to continue in a macro
    Signed-off-by: kalmenn <kalmenn@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,23 @@ use std::net::{TcpListener};
 mod request;
 mod varints;
 mod clients;
+mod codec;
 
 use clients::*;
 use request::*;
+
+// https://stackoverflow.com/questions/49785136/is-there-a-shortcut-to-unwrap-or-continue-in-a-loop
+macro_rules! continue_on_err {
+    ($res:expr) => {
+        match $res {
+            Ok(val) => val,
+            Err(e) => {
+                println!("Continued on error: {:?}", e);
+                continue;
+            }
+        }
+    };
+}
 
 fn main() {
     let mut clients = ClientCache::new();
@@ -13,18 +27,12 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = continue_on_err!(stream);
 
         clients.prune(2);
 
-        let addr = stream.peer_addr().unwrap();
-        let client = match clients.cache(addr) {
-            Ok(client) => client,
-            Err(err) => {
-                eprintln!("{:?}", err);
-                continue
-            }
-        };
+        let addr = continue_on_err!(stream.peer_addr());
+        let client = continue_on_err!(clients.cache(addr));
 
         handle_connection(stream, client);
         println!("current cached clients: {:#?}", &clients);
